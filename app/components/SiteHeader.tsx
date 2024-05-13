bb7cdac973aad3b3b87269774be38c75833e54b3
Niklas Andreasson <git@niftybits.se>
2024-05-13 14:22:38 +0200

    Improve SiteHeader responsiveness

--- app/components/SiteHeader.tsx
+++ app/components/SiteHeader.tsx
@@ -4,7 +4,7 @@ import { HeaderSearchBar } from "./HeaderSearchBar";
 
 export function SiteHeader() {
   return (
-    <header className="relative flex flex-none flex-wrap items-center border-black border-b-[40px] justify-between px-4 py-5 shadow-md shadow-slate-900/5 transition duration-500 sm:px-6 lg:px-8 dark:shadow-none dark:bg-transparent bg-red-800">
+    <header className="relative flex flex-col items-center border-black border-b-[40px] pb-[80px] md:pb-8 justify-between px-4 py-5 shadow-md shadow-slate-900/5 transition duration-500 sm:px-6 lg:px-8 dark:shadow-none dark:bg-transparent bg-red-800">
       <div className="mx-auto sm:px-2 lg:px-8 xl:px-12 w-full max-w-7xl relative flex flex-col md:flex-row flex-grow basis-0 items-center space-between">
         <div className="">
           <h1 className="text-4xl text-white">
@@ -16,7 +16,7 @@ export function SiteHeader() {
 
         <HeaderSearchBar />
       </div>
-      <div className="rounded-full bg-white border-black border-[10px] w-32 h-32 absolute bottom-[-80px] left-[45%]"></div>
+      <div className="rounded-full bg-white border-black border-[10px] w-32 h-32 bottom-[-80px] absolute"></div>
     </header>
   );
 }
